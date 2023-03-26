ef379a3c3392f78bb7da7e5a8700af063d6b0cf0
Jack Shrader <96661942+jpshrader@users.noreply.github.com>
2023-03-26 07:04:44 -0400

    yeah

--- main.ts
+++ main.ts
@@ -1,34 +1,29 @@
-//import yargs from 'yargs';
+import yargs from 'yargs';
 
 import { getGithubApiClient } from './client/github-api-client.js';
 import { processInstructions } from './instructions/instruction-processor.js';
 import { parseInstructions } from './instructions/instructions-parser.js';
 
-// const argv = await yargs(process.argv.slice(2)).options({
-//     instructions: {
-//         alias: 'instructions',
-//         type: 'string',
-//         demandOption: true
-//     },
-//     accessToken: {
-//         alias: 'access_token',
-//         type: 'string',
-//         demandOption: true
-//     }
-// }).argv;
+const argv = await yargs(process.argv.slice(2)).options({
+    instructions: {
+        alias: 'instructions',
+        type: 'string',
+        demandOption: true
+    },
+    accessToken: {
+        alias: 'access_token',
+        type: 'string',
+        demandOption: true
+    }
+}).argv;
 
-const accessToken = process.argv[2];
-const ins = process.argv[3];
+const instructions = parseInstructions(argv.instructions);
+const client = getGithubApiClient(argv.accessToken);
 
-//console.log('processing instructions: ', ins);
-
-const instructions = parseInstructions(ins);
-// const client = getGithubApiClient(accessToken);
-
-// const err = await processInstructions(client, instructions);
-// if (err) {
-//     console.error('encountered error processing instructions: ', err);
-//     process.exit(1);
-// }
+const err = await processInstructions(client, instructions);
+if (err) {
+    console.error('encountered error processing instructions: ', err);
+    process.exit(1);
+}
 
 console.log('instructions completed successfully');
