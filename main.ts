5e39404c09aebb39a3d231adbf69705f23dbdd85
Jack <96661942+jpshrader@users.noreply.github.com>
2024-05-15 11:13:29 -0400

    upgrade to node 22

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
+import { exit, argv } from 'process';
 import yargs from 'yargs';
 
 import { getGithubApiClient } from './client/github-api-client.js';
 import { processInstructions } from './instructions/instruction-processor.js';
 import { parseInstructions } from './instructions/instructions-parser.js';
 
-const argv = await yargs(process.argv.slice(2)).options({
+const args = await yargs(argv.slice(2)).options({
     instructions: {
         alias: 'instructions',
         type: 'string',
@@ -17,13 +18,13 @@ const argv = await yargs(process.argv.slice(2)).options({
     }
 }).argv;
 
-const instructions = parseInstructions(argv.instructions);
-const client = getGithubApiClient(argv.accessToken);
+const instructions = parseInstructions(args.instructions);
+const client = getGithubApiClient(args.accessToken);
 
 const err = await processInstructions(client, instructions);
 if (err) {
     console.error('encountered error processing instructions: ', err);
-    process.exit(1);
+    exit(1);
 }
 
 console.log('instructions completed successfully');
